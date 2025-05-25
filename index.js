bf72faa4c32019414bacccf4c5d8f97951677765
I_owned_you <mrkgitonga@gmail.com>
2025-05-25 14:40:57 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// How to write a comment in Javascript
+let name = 'Mark';
+console.log(name)
+// Cannot be a reserved keywork like if var etc
+// should ne a meaninigfull not like a  b v 
+// cannot statr with a number (1name)
+//cannot contain a space or hyphen(-)
+//are case-sensitive
+
+let firstName = 'Mark';
+let lastName ='Gitonga'; // By let you can re assign a code but by const you cant re assign a code
+
+const bestFood = '80';
+bestFood = '100';
