c90d53e51f283cdf99531ff6b5e9219713e17df6
I_owned_you <mrkgitonga@gmail.com>
2025-05-26 19:24:07 +0300

    n

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // How to write a comment in Javascript
-let name = 'Mark';
+let name = 'Mark'; //string
 console.log(name)
 // Cannot be a reserved keywork like if var etc
 // should ne a meaninigfull not like a  b v 
@@ -10,5 +10,12 @@ console.log(name)
 let firstName = 'Mark';
 let lastName ='Gitonga'; // By let you can re assign a code but by const you cant re assign a code
 
-const bestFood = '80';
-bestFood = '100';
+//const bestFood = '80';
+//bestFood = '100'; // by this it will give me an error saying it is in line 14 y/index.js:14
+
+let namse = 'Mark'; //strings
+let age = 30; //Number
+let isApproved = false; //Boolean
+let firstNames = undefined;
+let selectedColor = null; // can be set to red but null clears 
+
