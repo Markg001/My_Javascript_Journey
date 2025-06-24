62cdea0fc3105c7146cf0a1883fb2f50ed0b5705
I_owned_you <mrkgitonga@gmail.com>
2025-06-24 15:31:48 +0300

    Genesis of JS

---
+++ Genesis/Genesis.js
@@ -0,0 +1,7 @@
+let js = 'amazing';
+        if (js === 'amazing') alert('javascript is fun');
+
+console.log (40 + 9 + 34 - 10);
+// Values and Variables
+console.log ('Jabaa');
+console.log ('51');
