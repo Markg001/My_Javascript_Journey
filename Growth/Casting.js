5cd6cf3a5f70f44481d8d012664b6390fbc7cff7
I_owned_you <mrkgitonga@gmail.com>
2025-07-14 21:28:30 +0300

    Create Casting.js

---
+++ Growth/Casting.js
@@ -0,0 +1,39 @@
+// DATA CASTING 
+// This is converting data from one type to another in JavaScript
+//integer casting
+let a = 123;
+console.log(a);
+console.log(typeof a);
+let b = String(a);
+console.log(b);
+console.log( typeof b);
+
+//boolean casting
+let m = true;
+console.log(m);
+console.log(typeof m);
+let n = String(m);
+console.log(n);
+console.log(typeof n); 
+//number casting
+let c = "456";  
+console.log(c);
+console.log(typeof c);
+let d = Number(c);
+console.log(d); 
+console.log(typeof d);
+
+let x = ".456Nop";  
+console.log(x);
+console.log(typeof x);
+let y = parseInt(x);
+console.log(y); 
+console.log(typeof y);
+
+// boolean to Number
+let p = true;
+console.log(p);
+console.log(typeof p);
+let q = Number(p);
+console.log(q);
+console.log(typeof q);
